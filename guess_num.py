e75c739f2fd207fc0bbb11d64ca6fc9422381293
xm35p4ru6 <xm35p4ru6@yahoo.com.tw>
2022-08-30 21:33:29 +0800

    最終版

--- guess_num.py
+++ guess_num.py
@@ -1,10 +1,14 @@
 import random
+s = input('請輸入開始值')
+e = input('請輸入結束值')
+s = int(s)
+e = int(e)
 
-r = random.randint(1, 100)
+r = random.randint(s, e)
 c = 0
 while True:
     c += 1 # c = c + 1 (快寫法)
-    s = input('1~100, 請輸入一個數字: ')
+    s = input('請輸入一個數字: ')
     s = int(s)
     print('猜第', c, '次')
     if s == r:
