3430e1db942518fe2adf4e8b381afb496e3fc207
xm35p4ru6 <xm35p4ru6@yahoo.com.tw>
2022-08-30 21:13:55 +0800

    第一次更新

--- guess_num.py
+++ guess_num.py
@@ -1,9 +1,12 @@
 import random
 
 r = random.randint(1, 100)
+c = 0
 while True:
+    c += 1 # c = c + 1 (快寫法)
     s = input('1~100, 請輸入一個數字: ')
     s = int(s)
+    print('猜第', c, '次')
     if s == r:
         print('恭喜你猜中了!!')
         break
